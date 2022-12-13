87abcc2de6ff1f2eed2ab0f82097be0419dd9bb8
Dejan Petrovic <dejan.petrovic@startelecom.ca>
2022-12-13 12:44:10 +0100

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Application.Current.MainPage.ShowPopupAsync<SomePopup>(new SomePopup());
+    }
+}
+
---
+++ MauiApp1/SomePopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1;
+
+public partial class SomePopup : Popup
+{
+	public SomePopup()
+	{
+		InitializeComponent();
+	}
+}
